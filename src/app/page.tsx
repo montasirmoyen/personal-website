29b2d5a047ba8bb7af157fb0e8068c680edb9cbc
montasirmoyen <montasirmoyen@gmail.com>
2025-10-16 17:46:27 -0400

    Format &apos;

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,8 @@ export default function Home() {
   return (
     <PageTemplate backgroundImage="/background.jpg">
       <p className="text-md mt-5">Hi there!</p>
-      <p className="text-md mt-5">I'm Montasir Moyen, a full-stack software developer currently studying Computer Science at Suffolk University in Boston, Massachusetts.</p>
-      <p className="text-md mt-5">I've worked on various projects that range from websites, video games and iOS applications. Some of my most recent projects were based off my interests as I continued learning more about the respective technologies they required.</p>
+      <p className="text-md mt-5">I&apos;m Montasir Moyen, a full-stack software developer currently studying Computer Science at Suffolk University in Boston, Massachusetts.</p>
+      <p className="text-md mt-5">I&apos;ve worked on various projects that range from websites, video games and iOS applications. Some of my most recent projects were based off my interests as I continued learning more about the respective technologies they required.</p>
       <p className="text-md mt-5">If you would like to reach out to me, my email is always available.</p>
     </PageTemplate>
   );
