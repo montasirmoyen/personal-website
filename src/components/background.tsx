0de7dc2c349d222953abb0697ff0e97ccba24880
montasirmoyen <montasirmoyen@gmail.com>
2025-10-18 16:44:14 -0400

    Converted `<a>` element to `<Link />` from `next/link`

--- src/components/background.tsx
+++ src/components/background.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 
 type BackgroundProps = {
   backgroundImage: string;
@@ -17,24 +18,24 @@ export default function Background({ backgroundImage, children }: BackgroundProp
         <div className="flex items-center justify-between">
           <h1 className="text-3xl font-bold">Montasir Moyen</h1>
           <nav className="flex space-x-6 text-md font-semibold">
-            <a 
+            <Link 
               href="/" 
               className="text-white hover:text-gray-300 transition-colors duration-200 font-medium"
             >
               About
-            </a>
-            <a 
+            </Link>
+            <Link 
               href="/experience" 
               className="text-white hover:text-gray-300 transition-colors duration-200 font-medium"
             >
               Experience
-            </a>
-            <a 
+            </Link>
+            <Link 
               href="/education" 
               className="text-white hover:text-gray-300 transition-colors duration-200 font-medium"
             >
               Education
-            </a>
+            </Link>
           </nav>
         </div>
         {children}
