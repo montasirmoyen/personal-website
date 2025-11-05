2bd908da494d19644bac2b6b3d536206d9a2a092
montasirmoyen <montasirmoyen@gmail.com>
2025-11-04 21:30:15 -0500

    Improve wording

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,19 +6,24 @@ export default function Home() {
     <Background backgroundImage="/background.jpg">
       <div className="flex items-center gap-2">
         <Lightbulb className="w-4 h-4 sm:w-5 sm:h-5" />
-        <h1 className="text-lg sm:text-xl font-bold"> Hi there, I&apos;m Montasir. Welcome to my website!</h1>
+        <h1 className="text-lg sm:text-xl font-bold">Hi there, I&apos;m Montasir.</h1>
       </div>
       <hr
         className="mt-4"
         style={{ borderColor: "white", borderWidth: "1px", borderStyle: "solid" }}
       />
       <div className="text-sm sm:text-md font-medium">
-        <p className="mt-4 sm:mt-5">I also go by Monty, I&apos;m a full-stack software developer currently studying Computer Science at Suffolk University in Boston, Massachusetts.</p>
-        <p className="mt-4 sm:mt-5">I&apos;ve worked on various projects that range from websites, video games and macOS, iOS & Android applications.
-          Most of my projects were based off my interests as I continued learning more about the respective technologies they required.</p>
-        <p className="mt-4 sm:mt-5">But above all, I&apos;m always trying to learn and be diverse with my work.
-          Feel free to explore my website to learn more about myself!</p>
-        <p className="mt-4 sm:mt-5">If you would like to reach out to me, my emails are always available:</p>
+        <p className="mt-3 sm:mt-4">I also go by Monty, I&apos;m a full-stack software developer currently studying Computer Science at Suffolk University in Boston, Massachusetts.</p>
+        <p className="mt-3 sm:mt-4">My fascination with technology began in middle school, when I got my very first computer from my parents.</p> 
+        <p className="mt-3 sm:mt-4">
+            Nowadays, I spend a lot of time learning and building things like websites, video games, macOS tools, and mobile apps.
+            Most of my projects are inspired by personal interests and a curiosity to dive deeper into the technologies behind them.
+        </p>
+        <p className="mt-3 sm:mt-4">
+            Above all, I&apos;m always looking for ways to learn, grow, and improve my skills further.
+            Feel free to explore my site and learn more about myself!
+        </p>
+        <p className="mt-3 sm:mt-4">If you would like to reach out to me, my emails are always available:</p>
         <p className="text-blue-400 underline text-sm sm:text-base break-all">rmm05890@su.suffolk.edu</p>
         <p className="text-blue-400 underline text-sm sm:text-base break-all">montasirmoyen@gmail.com</p>
       </div>
