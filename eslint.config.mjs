86439ad8941b1dcc54ba18bd7a638b8a1714883f
montasirmoyen <montasirmoyen@gmail.com>
2025-11-19 21:18:43 -0500

    Update lint bypasses

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,7 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
+/* eslint-disable react/no-unescaped-entities */
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
