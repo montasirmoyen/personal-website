e84a365727991283dd14379073bfcf1c4f19afcc
Montasir Moyen <montasirmoyen@gmail.com>
2025-11-29 01:31:54 -0500

    Update homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,32 +1,20 @@
-import Background from "../components/background";
-import { Lightbulb } from "lucide-react"
+"use client";
+
+import Image from "next/image";
+import AuroraBackground from "../components/AuroraBackground";
 
 export default function Home() {
   return (
-    <Background backgroundImage="/background.jpg">
-      <div className="flex items-center gap-2">
-        <Lightbulb className="w-4 h-4 sm:w-5 sm:h-5" />
-        <h1 className="text-lg sm:text-xl font-bold">Hi there, I&apos;m Montasir.</h1>
-      </div>
-      <hr
-        className="mt-4"
-        style={{ borderColor: "white", borderWidth: "1px", borderStyle: "solid" }}
-      />
-      <div className="text-sm sm:text-md font-medium">
-        <p className="mt-3 sm:mt-4">I also go by Monty, I&apos;m a full-stack software developer currently studying Computer Science at Suffolk University in Boston, Massachusetts.</p>
-        <p className="mt-3 sm:mt-4">My fascination with technology began in middle school, when I got my very first computer from my parents.</p> 
-        <p className="mt-3 sm:mt-4">
-            Nowadays, I spend a lot of time learning and building things like websites, video games, macOS tools, and mobile apps.
-            Most of my projects are inspired by personal interests and a curiosity to dive deeper into the technologies behind them.
+    <AuroraBackground>
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <h1 className="text-5xl font-bold">Quietly building for the web...</h1>
+        <p className="mt-4 text-lg text-center max-w-2xl flex items-center justify-center gap-4">
+          Hello, I'm
+          <Image unoptimized className="rounded-3xl" src="/montasirmoyen.png" alt="Montasir Moyen Logo"
+            width={48} height={48} />
+          Montasir Moyen, a full-stack developer.
         </p>
-        <p className="mt-3 sm:mt-4">
-            Above all, I&apos;m always looking for ways to learn, grow, and improve my skills further.
-            Feel free to explore my site and learn more about myself!
-        </p>
-        <p className="mt-3 sm:mt-4">If you would like to reach out to me, my emails are always available:</p>
-        <p className="text-blue-400 underline text-sm sm:text-base break-all">rmm05890@su.suffolk.edu</p>
-        <p className="text-blue-400 underline text-sm sm:text-base break-all">montasirmoyen@gmail.com</p>
       </div>
-    </Background>
+    </AuroraBackground>
   );
 }
