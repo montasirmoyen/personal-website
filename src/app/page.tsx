f05d5ff9b4b881a2fb8975338c897f7465807215
montasirmoyen <montasirmoyen@gmail.com>
2025-11-29 23:36:26 -0500

    Update homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,19 +2,20 @@
 
 import Image from "next/image";
 import AuroraBackground from "../components/AuroraBackground";
+import { TextAnimate } from "../components/ui/text-animate"
 
 export default function Home() {
   return (
     <AuroraBackground>
       <div className="flex flex-col items-center justify-center min-h-screen">
-        <h1 className="text-5xl font-bold">Quietly building for the web...</h1>
-        <p className="mt-4 text-lg text-center max-w-2xl flex items-center justify-center gap-4">
-          Hello, I'm
-          <Image unoptimized className="rounded-3xl" src="/montasirmoyen.png" alt="Montasir Moyen Logo"
-            width={48} height={48} />
-          Montasir Moyen, a full-stack developer.
-        </p>
+        <TextAnimate animation="blurInUp" by="character" className="text-5xl font-bold font-hero">Quietly building for the web...</TextAnimate>
+        <TextAnimate animation="blurInUp" by="character" className="mt-4">
+          Hello, I'm Montasir Moyen, a full-stack developer.
+        </TextAnimate>
       </div>
     </AuroraBackground>
   );
 }
+
+{/* <Image unoptimized className="rounded-3xl" src="/montasirmoyen.png" alt="Montasir Moyen Logo"
+            width={48} height={48} /> */}
