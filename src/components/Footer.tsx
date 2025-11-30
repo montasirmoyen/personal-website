3bef9214b40fc222efd68f706df3124c774c079f
montasirmoyen <montasirmoyen@gmail.com>
2025-11-29 23:36:55 -0500

    Add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="bg-black/90 text-gray-400 text-center py-6 mt-12">
+            <p>&copy; {new Date().getFullYear()} Montasir Moyen. All rights reserved.</p>
+        </footer>
+    );
+}
