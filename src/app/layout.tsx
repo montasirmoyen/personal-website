348f997ceb6a2a9aa67cd0f4fcc25f124cebd213
Montasir Moyen <montasirmoyen@gmail.com>
2025-11-30 01:08:09 -0500

    Remove footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
         <div className="min-h-screen flex flex-col relative">
           <NavBar />
           <main className="flex-1">{children}</main>
-          <Footer />
+          
         </div>
       </body>
     </html>
