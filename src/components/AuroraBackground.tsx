495a8d05d7f88227ee6ca45b60bec1eb02f777b1
montasirmoyen <montasirmoyen@gmail.com>
2025-11-29 23:36:51 -0500

    Update aurora

--- src/components/AuroraBackground.tsx
+++ src/components/AuroraBackground.tsx
@@ -1,6 +1,8 @@
+import { Stars } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
 import { motion, useMotionTemplate, useMotionValue, animate } from "framer-motion";
 import React, { useEffect } from "react";
-const COLORS_TOP = ["#13FFAA", "#1E67C6", "#CE84CF", "#DD335C"];
+const COLORS_TOP = ["#00a295ff", "#2583b9ff", "#572faeff"];
 
 export default function AuroraBackground({ children }: { children: React.ReactNode }) {
  const color = useMotionValue(COLORS_TOP[0]);
@@ -22,6 +24,11 @@ export default function AuroraBackground({ children }: { children: React.ReactNo
       className="relative min-h-screen"
     >
       {children}
+      <div className="absolute inset-0 z-0">
+        
+      </div> {/*<Canvas>
+          <Stars count={500} factor={10} fade speed={.5} />
+        </Canvas> */}
     </motion.section>
   );
 }
