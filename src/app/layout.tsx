da1a71a169fae7a74ce775a17aed83f4a2900b92
monty <montasirmoyen@gmail.com>
2026-04-18 15:20:41 -0400

    Refactor RootLayout component to include TooltipProvider and restructure children rendering

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,15 @@
-import { Geist, Geist_Mono, DM_Sans } from "next/font/google"
+import { Geist_Mono, DM_Sans } from "next/font/google"
 
 import "./globals.css"
 import { cn } from "@/lib/utils";
+import mm_metadata from "@/lib/metadata"
+
+import NavBar from "@/components/ui/navbar1";
+import Footer from "@/components/ui/footer1";
+import { Toaster } from "@/components/ui/sonner"
+import { TooltipProvider } from "@/components/ui/tooltip"
+
+export const metadata = mm_metadata;
 
 const dmSans = DM_Sans({subsets:['latin'],variable:'--font-sans'})
 
@@ -22,7 +30,12 @@ export default function RootLayout({
       className={cn("antialiased", fontMono.variable, "font-sans", dmSans.variable)}
     >
       <body>
-        {children}
+        <TooltipProvider>
+          <NavBar />
+          {children}
+          <Footer />
+        </TooltipProvider>
+        <Toaster />
       </body>
     </html>
   )
